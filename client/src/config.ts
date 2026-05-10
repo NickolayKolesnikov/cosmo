3f5320c61275e16f6a05aa2ccaf7f28987f2bd3c
Nikolay Kolesnikov <kolesnikov.nov@gmail.com>
2026-05-10 22:16:32 +0300

    Update client default game server host

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,4 +1,4 @@
-const serverHost = (import.meta.env.VITE_GAME_SERVER_HOST as string | undefined) ?? "192.168.1.56";
+const serverHost = (import.meta.env.VITE_GAME_SERVER_HOST as string | undefined) ?? "127.0.0.1";
 const serverPortRaw = (import.meta.env.VITE_GAME_SERVER_PORT as string | undefined) ?? "5001";
 const serverPort = Number(serverPortRaw);
 
